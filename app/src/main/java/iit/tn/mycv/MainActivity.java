c8a76477bf9f4a58af31a25c67e24809bbefc313
FirasAbdennadher <firasabdenandher82@gmail.com>
2020-05-13 18:04:30 +0100

    drawer navigate Done !

--- app/src/main/java/iit/tn/mycv/MainActivity.java
+++ app/src/main/java/iit/tn/mycv/MainActivity.java
@@ -1,15 +1,21 @@
 package iit.tn.mycv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity
+implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
 
     @Override
@@ -21,10 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView= findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar
                 , R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toogle);
         toogle.syncState();
+        if(savedInstanceState ==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new BioFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_bio);
+
+        }
     }
 
     @Override
@@ -34,5 +48,20 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.nav_bio:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new BioFragment()).commit();
+                break;
+            case R.id.nav_formation:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FormationFragment()).commit();
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
