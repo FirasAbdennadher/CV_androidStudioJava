c4e929dc8804391a8e532bb4790e858267d83d4b
FirasAbdennadher <firasabdenandher82@gmail.com>
2020-05-17 01:18:52 +0100

    start with formation

--- app/src/main/java/iit/tn/mycv/BioFragment.java
+++ app/src/main/java/iit/tn/mycv/BioFragment.java
@@ -14,13 +14,15 @@ import androidx.fragment.app.Fragment;
 
 public class BioFragment extends Fragment implements View.OnClickListener {
     private Button linkedin;
-View view;
+    View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        view=inflater.inflate(R.layout.fragment_bio,container,false);
-        linkedin=(Button)view.findViewById(R.id.linkedin);
+        view = inflater.inflate(R.layout.fragment_bio, container, false);
+        linkedin = (Button) view.findViewById(R.id.linkedin);
+        linkedin.setOnClickListener(this);
         return view;
     }
 
@@ -29,15 +31,27 @@ View view;
         switch (v.getId()) {
 
             case R.id.linkedin:
-                ouvirLinkedIn();
+                ouvirAccount("https://www.linkedin.com/in/firas-abdennadher/");
+                break;
+            case R.id.codeforces:
+                ouvirAccount("http://codeforces.com/profile/firasabdennadher82");
+                break;
+            case R.id.github:
+                ouvirAccount("https://github.com/FirasAbdennadher/");
+                break;
         }
     }
 
-    private void ouvirLinkedIn() {
-        String profile_url = "https://www.linkedin.com/in/firas-abdennadher/";
+    private void ouvirAccount(String profile_url) {
+        //  String profile_url = "https://www.linkedin.com/in/firas-abdennadher/";
+        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(profile_url)));
+    }
+
+  /*  private void ouvirCodeForces() {
+        String profile_url = "http://codeforces.com/profile/firasabdennadher82";
         try {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url));
-            intent.setPackage("com.linkedin.android");
+            intent.setPackage("com.codeforces.android");
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         } catch (Exception e) {
@@ -45,4 +59,17 @@ View view;
         }
 
     }
+
+    private void ouvirGitHub() {
+        String profile_url = "https://github.com/FirasAbdennadher/";
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url));
+            intent.setPackage("com.github.android");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } catch (Exception e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url)));
+        }
+
+    }*/
 }
