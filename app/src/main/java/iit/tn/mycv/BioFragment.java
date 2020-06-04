def6af74ba0571c4b9490a29df84746da387467e
FirasAbdennadher <firasabdenandher82@gmail.com>
2020-06-04 12:57:39 +0100

    Bio Crtif Competence Formation Langue String

--- app/src/main/java/iit/tn/mycv/BioFragment.java
+++ app/src/main/java/iit/tn/mycv/BioFragment.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +17,8 @@ import androidx.fragment.app.Fragment;
 
 public class BioFragment extends Fragment implements View.OnClickListener {
     private Button linkedin;
+    private TextView bioid;
+
     View view;
 
     @Nullable
@@ -22,7 +27,12 @@ public class BioFragment extends Fragment implements View.OnClickListener {
         //return super.onCreateView(inflater, container, savedInstanceState);
         view = inflater.inflate(R.layout.fragment_bio, container, false);
         linkedin = (Button) view.findViewById(R.id.linkedin);
+        bioid=view.findViewById(R.id.bioid);
         linkedin.setOnClickListener(this);
+        Animation a =AnimationUtils.loadAnimation(getContext(),android.R.anim.slide_in_left);
+        bioid.setAnimation(a);
+        a.setDuration(1000);
+
         return view;
     }
 
@@ -31,45 +41,20 @@ public class BioFragment extends Fragment implements View.OnClickListener {
         switch (v.getId()) {
 
             case R.id.linkedin:
+               // ouvirAccount("https://www.linkedin.com/in/firas-abdennadher/");
+               // break;
+            case R.id.codeforces:
                 ouvirAccount("https://www.linkedin.com/in/firas-abdennadher/");
                 break;
-            case R.id.codeforces:
-                ouvirAccount("http://codeforces.com/profile/firasabdennadher82");
-                break;
-            case R.id.github:
-                ouvirAccount("https://github.com/FirasAbdennadher/");
+           case R.id.github:
+                ouvirAccount("https://www.linkedin.com/in/firas-abdennadher/");
                 break;
         }
     }
 
     private void ouvirAccount(String profile_url) {
-        //  String profile_url = "https://www.linkedin.com/in/firas-abdennadher/";
         startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(profile_url)));
     }
 
-  /*  private void ouvirCodeForces() {
-        String profile_url = "http://codeforces.com/profile/firasabdennadher82";
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url));
-            intent.setPackage("com.codeforces.android");
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        } catch (Exception e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url)));
-        }
 
-    }
-
-    private void ouvirGitHub() {
-        String profile_url = "https://github.com/FirasAbdennadher/";
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url));
-            intent.setPackage("com.github.android");
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        } catch (Exception e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(profile_url)));
-        }
-
-    }*/
 }
